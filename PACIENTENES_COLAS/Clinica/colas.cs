766cb13f12b3f5bbc10ea85950d67a8e9e1bd218
stevenheroku <herokujeff@gmail.com>
2021-02-13 09:10:08 -0600

    Agregar archivos de proyecto.

---
+++ PACIENTENES_COLAS/Clinica/colas.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace PACIENTENES_COLAS.Clinica
+{
+    class colas
+    {
+
+        //ATRIBUTOS PARA CLINICA
+        string[] cola;
+        int primerlemento, elementofinal, tam;
+
+        //CONSTRUCTOR
+        public colas(int n)
+        {
+            primerlemento = elementofinal = -1;
+            tam = n;
+            cola = new string[tam];
+        }
+
+        public bool esta_llena()
+        {
+            if (elementofinal >= tam - 1)
+                return true;
+            return false;
+        }
+
+        public bool esta_vacia()
+        {
+            if (primerlemento == -1)
+                return true;
+            return false;
+        }
+
+        public bool agregar( string nombre)
+        {
+            if (!esta_llena())
+            {
+
+               
+                cola[++elementofinal] = nombre;
+
+                if (elementofinal == 0)
+                    primerlemento = 0;
+                return true;
+            }
+            return false;
+        }
+
+        public bool extraer(ref string nombre)
+        {
+            //ESTA VRIABLE SE USARA PARA MOVER EL DATO 1 al 0, el 2 al 1, el 3 al 2 ...
+            int var = 1;
+
+            //SI LA PILA NO ESTA VACIA:
+            if (!esta_vacia())
+            {
+                //ENTONCES SACAMOS EL PRIMER DATO SERIA EL NOMBRE DEL PACIENTE 1 (P[O])
+                
+                nombre = cola[primerlemento];
+
+
+
+                for (int i = 0; i < cola.Length; i++)
+                {
+                    //SE PREGUNTA SI var ES MENOR A LA LONGITUD DEL ARREGLO, ESTO SE HACE DEBIDO A QUE var LLEGA A UN VALOR
+                    //SUPERIOR A LA LONGITUD DEL ARREGLO
+                    if (var < cola.Length)
+                    {
+                        //PASAMOS EL DATO DE UNA POSICION A OTRA
+                        //EJEMPLO: vec[0]=vec[1] EL DATO EN LA POSICION 1 SE PASA A LA POSICION 0 Y ASI HASTA LA LONGITUD DEL ARREGLO
+                        cola[i] = cola[var];
+                        var++;
+                    }
+                }
+
+                //AHORA LA VARIABLE QUE SE IRA MOVIENDO SERA u, YA QUE EL PRIMERO SIEMPRE SERA EL ELEMENTO 0
+                if (elementofinal == primerlemento)
+                {
+                    primerlemento = elementofinal = -1;
+                }
+                else
+                    //DECREMENTAMOS u YA QUE QUIERE DECIR QUE "HAY UN DATO MENOS EN LA COLA", EN REALIDAD EL DATO SIGUE ALLI
+                    elementofinal--;
+                return true;
+            }
+            return false;
+        }
+
+
+
+    }
+}
---
+++ PACIENTENES_COLAS/Program.cs
@@ -0,0 +1,100 @@
+﻿using PACIENTENES_COLAS.Clinica;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PACIENTENES_COLAS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int opcion = 0;
+            int n;
+            string nompaciente = "";
+            string apellidospac = "";
+            string direccion="";
+
+            //PEDIMOS CUANTOS PACIENTES VAMMOS AGREGAR A LA COLA
+            Console.Write("CUANTOS PACIETES DESEA INGRESAR A LA CLINICA *SALUDENT* ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            //HACEMOS LA COLA
+            colas cola = new colas(n);
+
+
+            do
+            {
+
+                Console.WriteLine("OPCIONES DE LA CLINICA   *SALUDENT*");
+                Console.WriteLine("1. AGREGAR PACIENTE: ");
+                Console.WriteLine("2. ATENDER PACIENTES E IRLOS ELIMINANDO CONFORME SU TURNO AGREGADO: ");
+                Console.WriteLine("3. Salir: \n");
+                opcion = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+
+                        int contadorpacientes = 1;
+
+                        //LE AGREGAMOS PACIENTES A LA COLA
+
+                        Console.WriteLine("\n AGREGAMOS LOS DATOS DEL PACIENTES");
+                        for (int i = 0; i < n; i++)
+                        {
+
+                            
+                            Console.WriteLine("\n Nombre del Paciente: "+ contadorpacientes);
+                            nompaciente = (Console.ReadLine());
+                            Console.WriteLine("Apellido del Paciente: " +contadorpacientes);
+                            apellidospac = (Console.ReadLine());
+
+
+                            if (cola.agregar( nompaciente))
+                            {
+                                Console.WriteLine("AGREGADO AL REGISTRO EL PACIENTE:  " + nompaciente+" "+apellidospac   );
+
+                            }
+                            else if (cola.esta_llena())
+                            {
+                                Console.WriteLine("YA ESTAN LLENOS LOS CUPOS DE LA CLINICA\n");
+                            }
+
+                            contadorpacientes++;
+                        }
+
+                        break;
+
+                    case 2:
+                        //LE EXTRAEMOS ELEMENTOS A LA COLA
+                        Console.WriteLine("\nATENDER PACIENTES  EN ORDEN");
+                        while (cola.extraer(ref nompaciente))
+                        {
+                            Console.WriteLine("DATOS DEL PACIENTE A ATENDER  NOMBRE:  " + nompaciente +"\n");
+                            Console.WriteLine("EL PACIENTE "+ nompaciente + " FUE ELIMINADO DE LA COLA\n");
+                            Console.WriteLine("ATENDER AL SIGUIENTE PACIENTE");
+                            Console.ReadLine();
+
+                            if (cola.esta_vacia())
+                            {
+                                Console.WriteLine("YA ESTAN ATENDIDOS TODOS LOS PACIENTES");
+                                Console.ReadLine();
+                            }
+
+                        }
+                        Console.ReadKey();
+                        break;
+                    case 0:
+
+                        break;
+                }
+
+            } while (opcion != 3);
+
+        }
+    }
+}
